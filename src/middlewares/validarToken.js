39b4ced8ac5f9b33d5fd26ab5a1145d78c1fbf3e
Maxi Ordoñez <exemaxi@outlook.com>
2025-12-14 08:16:49 -0300

    Se agrego el middleware para validar el token.

---
+++ src/middlewares/validarToken.js
@@ -0,0 +1,18 @@
+import jwt from "jsonwebtoken";
+
+const validarToken = (req, res, next) => {
+  try {
+    const token = req.header("x-token");
+    if (!token) {
+      return res.status(401).json({ mensaje: "No hay token en la petición" });
+    }
+    const payload = jwt.verify(token, process.env.SECRETA_JWT);
+    req.usuario = payload.usuario;
+    next();
+  } catch (error) {
+    console.log(error);
+    res.status(401).json({ mensaje: "Token no válido" });
+  }
+};
+
+export default validarToken;
