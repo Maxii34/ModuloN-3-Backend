199c0e5e5c0e95fc995e32f1edf3d76619254b85
Santiago Robledo <santiddf9@gmail.com>
2026-03-18 02:03:25 -0300

    solucione el problema de Error en la lógica de disponibilidad de reservas por fechas

--- src/controllers/reservas.controllers.js
+++ src/controllers/reservas.controllers.js
@@ -1,7 +1,6 @@
 import Reserva from "../models/reservas.js";
 import mongoose from "mongoose";
 
-
 export const crearReserva = async (req, res) => {
   try {
     console.log("REQ.USUARIO:", req.usuario);
@@ -10,6 +9,29 @@ export const crearReserva = async (req, res) => {
 
     if (!habitacion || !fechaEntrada || !fechaSalida || !cantidadHuespedes) {
       return res.status(400).json({ mensaje: "Faltan datos obligatorios" });
+    }
+
+    const ingreso = new Date(fechaEntrada);
+    const salida = new Date(fechaSalida);
+
+    if (salida <= ingreso) {
+      return res.status(400).json({ 
+        mensaje: "La fecha de salida debe ser posterior a la fecha de entrada" 
+      });
+    }
+
+    const reservaExistente = await Reserva.findOne({
+      habitacion: habitacion,
+      $and: [
+        { fechaEntrada: { $lt: salida } }, 
+        { fechaSalida: { $gt: ingreso } }  
+      ]
+    });
+
+    if (reservaExistente) {
+      return res.status(400).json({
+        mensaje: "La habitación no está disponible en las fechas seleccionadas. Por favor, elige otras."
+      });
     }
 
     const nuevaReserva = new Reserva({
@@ -31,7 +53,6 @@ export const crearReserva = async (req, res) => {
     res.status(500).json({ mensaje: "Error al crear la reserva" });
   }
 };
-
 
 export const obtenerMisReservas = async (req, res) => {
   try {
